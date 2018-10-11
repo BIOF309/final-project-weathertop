ebaa4e1fe77052282651b5ab01a30218851512c2
Andy Kehr <andrew.d.kehr@gmail.com>
2018-10-11 18:04:31 -0400

    Renamed sensor files Included imports of sensors to final compile

--- Compile data.py
+++ Compile data.py
@@ -2,5 +2,11 @@
 # store data as CSV
 # use loc to get info from particular time points (make sure to use a time structure taht makes a lot of sense
 
+import BME280_weathertop as THP
+import Rain as rain
+import Wind_Dir as wd
+import Wind_Speed as ws
+
+
 import pandas as pd
 pd.read_csv(filepath_or_buffer=)
